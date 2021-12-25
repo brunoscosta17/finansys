451aa08a5f705828a2d83484b72005d5dfd1322d
= <=>
2021-12-25 18:19:17 -0300

    adding EntryForm

---
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -0,0 +1,129 @@
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+import { Entry } from '../shared/entry.model';
+import { EntryService } from '../shared/entry.service';
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.sass']
+})
+export class EntryFormComponent implements OnInit, AfterContentChecked {
+
+  currentAction!: string;
+  form !: FormGroup;
+  pageTitle!: string;
+  serverErrorMessages: string[] = [];
+  submittingForm = false;
+  entry: Entry = new Entry();
+  id!: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private entryService: EntryService,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildEntryForm();
+    this.loadEntry();
+  }
+
+  ngAfterContentChecked(): void {
+    this.setPageTitle();
+  }
+
+  private setCurrentAction(): void {
+    this.route.snapshot.url[0].path === 'new' ? this.currentAction = 'new' : this.currentAction = 'edit';
+  }
+
+  private buildEntryForm(): void {
+    this.form  = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]],
+    });
+  }
+
+  private loadEntry(): void {
+    this.route.params.subscribe(params => this.id = params.id);
+    if (this.currentAction === 'edit') {
+      this.entryService.getById(this.id)
+        .subscribe(entry => {
+          this.entry = entry;
+          this.form .patchValue(this.entry);
+        }, (e) => this.toastr.error(e));
+    }
+  }
+
+  private setPageTitle(): void {
+    if (this.currentAction === 'new') {
+      this.pageTitle = 'Cadastro de novo lançamento';
+    } else {
+      const entryName = this.entry.name || '';
+      this.pageTitle = 'Editando lançamento: ' + entryName;
+    }
+  }
+
+  private createEntry(): void {
+    const entry: Entry = Object.assign(new Entry(), this.form .value);
+    this.entryService.create(entry)
+      .subscribe(
+        // tslint:disable-next-line: no-shadowed-variable
+        entry => this.actionsForSuccess(entry),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private updateEntry(): void {
+    const entry: Entry = Object.assign(new Entry(), this.form .value);
+    this.entryService.update(entry)
+      .subscribe(
+        // tslint:disable-next-line: no-shadowed-variable
+        entry => this.actionsForSuccess(entry),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private actionsForSuccess(entry: Entry): void {
+    if (this.currentAction === 'edit') {
+      this.toastr.success('Lançamento atualizado com sucesso!');
+      this.router.navigate(['/entries']);
+    } else {
+      this.toastr.success('Lançamento criado com sucesso!');
+      this.router.navigate(['/entries']);
+    }
+  }
+
+  private actionsForError(error: any): void {
+    this.toastr.error('Erro ao salvar lançamento!');
+    this.submittingForm = false;
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor.'];
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  handleSubmit(): void {
+    this.submittingForm = true;
+    if (this.currentAction === 'new') {
+      this.createEntry();
+    } else {
+      this.updateEntry();
+    }
+  }
+
+}
